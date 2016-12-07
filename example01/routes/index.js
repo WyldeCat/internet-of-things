fa34c35691a0bc0d6e78d9d96f4816203287cea1
wyldecat <skan1543@gmail.com>
2016-12-07 13:22:09 +0900

    Implement authentication

--- example01/routes/index.js
+++ example01/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Expression~~' });
 });
 
+router.post('/do-authentication',function(req,res,next){
+  if(req.body.id === 'test' && req.body.pswd === 'test') {
+    res.send('<script type="text/javascript">alert("Hi me!!");window.location.href="/secure"</script>');
+  } else {
+    res.send('<script type="text/javascript">alert("you are not me.. right?");window.location.href = "/";</script>');
+  }
+});
+
 module.exports = router;
